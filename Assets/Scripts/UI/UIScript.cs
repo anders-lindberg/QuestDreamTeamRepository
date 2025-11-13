f9deb7e1ce23222f2bb0eb412803cb0450279979
anders-lindberg <4nderslindberg@gmail.com>
2025-11-13 11:46:29 +0100

    ryry message på main

---
+++ Assets/Scripts/UI/UIScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    void Starthjh()
+    {
+        
+    }
+
+    //  ændring i linje er vist som blå linje til venstre
+    void Update()
+    {
+
+    }
+    //testing new changed testing af inline view
+    //2222 test2222
+    //this is my new feature
+    //rød trekant indikerer hvis en linje er blevet fjernet helt, grøn er ny linje
+    // hej axelyryryrryr
+}
