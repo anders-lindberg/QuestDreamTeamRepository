be78d76350edfc113f9f6e9213f292d776b75e04
Axel Bernhard Dybdal Olin <axelolin5@gmail.com>
2025-11-24 14:41:35 +0100

    gamemanager beginning

---
+++ Assets/Scripts/Managers/Game/GameManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    //singleton logic
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //variables that keep track of the different tnt parts in the levels:
+    public bool level1TNT;
+    public bool level2TNT;
+    public bool level3TNT;
+
+
+    //Counts how many TNT parts are collected in total by adding +1 for each level that has its TNT collected.
+    //Same as saying "if level1TNT = true {tntcollected += 1} else {tntcollected += 0} etc."
+    public int tNTCollected => (level1TNT ? 1:0) + (level2TNT ? 1: 0) + (level3TNT ? 1:0);
+
+
+    //method that should be called when collecting a tnt part
+    public void TNTCollected(int levelIndex)
+    {
+        //based on the given levelindex, the corresponding bool should be set to true, so the manager knows which parts the player has collected (or not)
+        switch(levelIndex)
+        {
+            case 1: level1TNT = true; break;
+
+            case 2: level2TNT = true; break;
+
+            case 3: level3TNT = true; break;
+        }
+    }
+
+
+    public bool HasQuestBeenCompleted()
+    {
+        //all three variables must be true for the method to return true, meaning: "HasQuestBeenCompleted()? == true (yes)"
+        return level1TNT && level2TNT && level3TNT;
+    }
+}
---
+++ Assets/Scripts/Managers/Game/TNTOnPickup.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TNTOnPickup : MonoBehaviour
+{
+    [SerializeField] private int tntIndex;
+
+    private void Start()
+    {
+        if (GameManager.Instance == null) return;
+
+        //checks if the player has already picked up the tnt part in the scene. If this is the case, it destroys the tnt part in the scene
+        if(tntIndex == 1 && GameManager.Instance.level1TNT || tntIndex == 2 && GameManager.Instance.level2TNT || tntIndex == 3 && GameManager.Instance.level3TNT)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.Instance.TNTCollected(tntIndex);
+            Destroy(gameObject);
+        }
+    }
+}
