99d266c4703919e30b64c76a5b0c370355dae94c
anders-lindberg <4nderslindberg@gmail.com>
2025-11-26 08:49:44 +0100

    kf

--- Assets/Scripts/Player/Movement/NoPickaxe.cs
+++ Assets/Scripts/Player/Movement/NoPickaxe.cs
@@ -1,6 +1,5 @@
 using JetBrains.Annotations;
 using Unity.VisualScripting;
-using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
--- Assets/Scripts/UI/PlaceItem.cs
+++ Assets/Scripts/UI/PlaceItem.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 public class PlaceItem : MonoBehaviour
 {
     
